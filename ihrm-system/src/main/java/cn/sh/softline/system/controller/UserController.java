39e5fe3ce3ef94c266747f12c78b7adf461613c1
jiu.hongyang <403096534@qq.com>
2020-06-10 11:35:43 +0800

    添加角色权限服务

--- ihrm-system/src/main/java/cn/sh/softline/system/controller/UserController.java
+++ ihrm-system/src/main/java/cn/sh/softline/system/controller/UserController.java
@@ -5,13 +5,16 @@ import cn.sh.softline.common.entity.Result;
 import cn.sh.softline.exception.CommonException;
 import cn.sh.softline.system.entity.User;
 import cn.sh.softline.system.service.UserService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author rz045
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
  */
 @CrossOrigin
 @RestController
+@Api(value = "用户管理", tags = "用户管理")
 @RequestMapping("/system")
 public class UserController {
 
@@ -27,24 +31,47 @@ public class UserController {
 
     @PostMapping("add")
     @ApiOperation(value = "用户新增")
-    public Result add(@RequestBody User user){
+    public Result add(@RequestBody User user) {
         boolean save = userService.save(user);
-        if(save){
+        if (save) {
             return Result.SUCCESS();
-        }else{
+        } else {
             return Result.FAIL();
         }
     }
 
+    @ApiOperation(value = "删除用户")
     @DeleteMapping("delete/{id}")
-    public Result delete(@PathVariable("id") String id){
+    public Result delete(@PathVariable("id") String id) {
         boolean res = userService.removeById(id);
-        if(res){
+        if (res) {
             return Result.SUCCESS();
-        }else{
+        } else {
             return Result.FAIL();
         }
     }
 
+    @PutMapping("update")
+    public Result update(@RequestBody User user) {
+        boolean update = userService.update(user, null);
+        if (update) {
+            return Result.SUCCESS();
+        } else {
+            return Result.FAIL();
+        }
+    }
 
+    @GetMapping("select/{id}")
+    public Result queryById(@PathVariable("id") String id) {
+        User user = userService.getById(id);
+        return Result.SUCCESS(user);
+    }
+
+    @GetMapping("select")
+    public Result queryInfo(@RequestParam("pageNo") long pageNo, @RequestParam("size") long size, @RequestBody User user) {
+        Page page = new Page(pageNo,size);
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        //todo 没写完
+        return null;
+    }
 }
