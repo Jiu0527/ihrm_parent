d93026ededed62bcdcf24e99682513db5190fcb4
jiu.hongyang <403096534@qq.com>
2020-06-24 15:49:11 +0800

    添加jwt

--- ihrm-system/src/main/java/cn/sh/softline/system/SystemApplication.java
+++ ihrm-system/src/main/java/cn/sh/softline/system/SystemApplication.java
@@ -1,8 +1,10 @@
 package cn.sh.softline.system;
 
+import cn.sh.softline.common.utils.JwtUtils;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("cn.sh.softline.system.mapper")
@@ -10,4 +12,5 @@ public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class,args);
     }
+
 }
--- ihrm-system/src/main/java/cn/sh/softline/system/controller/UserController.java
+++ ihrm-system/src/main/java/cn/sh/softline/system/controller/UserController.java
@@ -2,6 +2,8 @@ package cn.sh.softline.system.controller;
 
 
 import cn.sh.softline.common.entity.Result;
+import cn.sh.softline.common.entity.ResultCode;
+import cn.sh.softline.common.utils.JwtUtils;
 import cn.sh.softline.exception.CommonException;
 import cn.sh.softline.system.entity.User;
 import cn.sh.softline.system.service.UserService;
@@ -11,6 +13,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.ResultSetMetaData;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -28,6 +35,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private JwtUtils jwtUtils;
 
     @PostMapping("add")
     @ApiOperation(value = "用户新增")
@@ -69,21 +78,43 @@ public class UserController {
 
     @GetMapping("select")
     public Result queryInfo(@RequestParam("pageNo") long pageNo, @RequestParam("size") long size, @RequestBody User user) {
-        Page page = new Page(pageNo,size);
+        Page page = new Page(pageNo, size);
         QueryWrapper<User> wrapper = new QueryWrapper<>();
-        if(user.getId() != null){
-            wrapper.eq("id",user.getId());
+        if (user.getId() != null) {
+            wrapper.eq("id", user.getId());
         }
-        if(user.getMobile() != null){
-            wrapper.eq("mobile",user.getMobile());
+        if (user.getMobile() != null) {
+            wrapper.eq("mobile", user.getMobile());
         }
-        if(user.getDepartmentId() != null){
-            wrapper.eq("departmentId",user.getDepartmentId());
+        if (user.getDepartmentId() != null) {
+            wrapper.eq("departmentId", user.getDepartmentId());
         }
-        if(user.getFormOfEmployment() != null){
-            wrapper.eq("formOfEmployment",user.getFormOfEmployment());
+        if (user.getFormOfEmployment() != null) {
+            wrapper.eq("formOfEmployment", user.getFormOfEmployment());
         }
         Page list = userService.page(page, wrapper);
         return Result.SUCCESS(list);
+    }
+
+    @PostMapping("/login")
+    public Result login(@RequestBody Map<String, String> loginMap) {
+        String mobile = loginMap.get("mobile");
+        String password = loginMap.get("password");
+        QueryWrapper<User> wrapper = new QueryWrapper();
+        if (password != null) {
+            wrapper.eq("mobile", mobile);
+        }
+        List<User> list = userService.list(wrapper);
+        User user = list.get(0);
+        if (user == null|| !user.getPassword().equals(password)) {
+            return new Result(ResultCode.MOBILEANDPASSWORDERROR);
+        }else{
+            Map<String,Object> map = new HashMap<>();
+            map.put("companyId",user.getCompanyId());
+            map.put("companyName",user.getCompanyName());
+            String token = jwtUtils.createJWT(user.getId(), user.getUsername(), map);
+            return new Result(ResultCode.SUCCESS,token);
+        }
+
     }
 }
